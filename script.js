fe7caef5528cae62c14ba5ac4286f82625031b85
pleuni <pleuniveen@gmail.com>
2023-12-17 13:45:21 +0100

    dropdown works

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  function showlist() {
+    var content = document.getElementById("drpdcontent");
+    if (content.style.display === 'none') {
+      content.style.display = 'block';
+    } else {
+      content.style.display = 'none';
+    }
+  }
+  
